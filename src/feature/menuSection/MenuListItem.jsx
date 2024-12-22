c98e1a283a79d5ae88d6f66a8fcf3c3f253057b4
fatemehmaghsoodi <fatemehhmaghsoodi@gmail.com>
2024-12-22 19:07:56 +0330

    change menu images

--- src/feature/menuSection/MenuListItem.jsx
+++ src/feature/menuSection/MenuListItem.jsx
@@ -6,7 +6,7 @@ function MenuListItem({item, index, categoryHandler}) {
   return (
     <li  className="p-5 w-24 h-fit text-center flex justify-center items-center" >
       <button onClick={()=>categoryHandler(item.name)} onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}>
-          <img className='mb-2 max-sm:w-12' src={isHover? `/public/images/${item.img}-icon-hover-menu${index+1}.png`:`/public/images/${item.img}-icon-menu${index+1}.png`} alt={item.name}/>
+          <img className='mb-2 max-sm:w-12' src={isHover? `/images/${item.img}-icon-hover-menu${index+1}.png`:`/images/${item.img}-icon-menu${index+1}.png`} alt={item.name}/>
           <span className={isHover?`text-secondary-200`: `` }>{item.name}</span>
       </button>
     </li>
